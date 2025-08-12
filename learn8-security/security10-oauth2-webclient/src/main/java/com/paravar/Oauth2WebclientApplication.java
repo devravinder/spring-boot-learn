c17dee9c9f5ec4463294e054260f8ab953ce61d8
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2025-08-12 16:48:55 +0530

    feat: add spring security

---
+++ learn8-security/security10-oauth2-webclient/src/main/java/com/paravar/Oauth2WebclientApplication.java
@@ -0,0 +1,11 @@
+package com.paravar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WebclientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2WebclientApplication.class, args);
+    }
+}
---
+++ learn8-security/security7-jwt-basic/src/main/java/com/paravar/JwtBasicSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.paravar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBasicSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JwtBasicSecurityApplication.class, args);
+    }
+}
